c4d8d167cb3c957bd8ea7b12efc4e2f2c44995c1
Meredith <meredith@getsentry.com>
2016-11-08 15:05:15 -0800

    sentry initializer

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,3 @@
-# Raven.configure do |config|
-#   config.dsn = 'https://e6e9e19d380c4b6380a0d8bd924a5602:1c4c3678e3e1401ba527cd8e7638c73d@sentry.io/106346'
-# end
+Raven.configure do |config|
+  config.dsn = 'https://e6e9e19d380c4b6380a0d8bd924a5602:1c4c3678e3e1401ba527cd8e7638c73d@sentry.io/106346'
+end
