46204b88507301213e3e0f1f0c32ed123c44df0d
Meredith <meredith@getsentry.com>
2016-11-08 14:56:15 -0800

    changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,16 +2,30 @@ class WelcomeController < ApplicationController
 
   def index
     @user = "meredith"
-    begin
-      1 / 0
-    rescue StandardError => detail
-      Raven.captureException(detail)
-      flash[:sentry_event_id] = Raven.last_event_id()
-    else
-      render :index
-    end
+    # begin
+    #   1 / 0
+    # rescue StandardError => detail
+    #   Raven.captureException(detail)
+    #   flash[:sentry_event_id] = Raven.last_event_id()
+    # else
+    #   render :index
+    # end
+    render :index
   end
 
   def show
+    begin
+      raise "meow"
+    rescue => detail
+      Raven.annotate_exception(detail, {
+          :user => {'person' => 1},
+          tags: {
+            meow: "cat"
+          }
+        })
+      Raven.captureException(detail)
+    else
+      render :show
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RubyRailsSample
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    Raven.configure do |config|
+      config.dsn = 'https://e6e9e19d380c4b6380a0d8bd924a5602:1c4c3678e3e1401ba527cd8e7638c73d@sentry.io/106346'
+    end
   end
 end
