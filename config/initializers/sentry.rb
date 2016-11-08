5566be42ed30901f2302cad2969d05814f32fe71
Meredith <meredith@getsentry.com>
2016-11-08 15:27:15 -0800

    more testing

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
 Raven.configure do |config|
   config.dsn = 'https://e6e9e19d380c4b6380a0d8bd924a5602:1c4c3678e3e1401ba527cd8e7638c73d@sentry.io/106346'
-  config.current_environment = 'production'
+  # config.current_environment = 'production'
 end
